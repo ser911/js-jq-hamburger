c78d86f7475005ff3c94774948256cf9b8b40c1b
Sergiomarretta <sergio.marretta@libero.it>
2021-03-11 17:32:11 +0100

    Fade-out

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
-var hamburger = $("i");
+var hamburger = $("i.fas.fa-bars");
 var hamburgerMenu = $(".hamburger-menu");
+var exit = $("i.fas.fa-times");
 
 hamburger.click(
 function()
 {
-  hamburgerMenu.fadeIn(1500);
+  hamburgerMenu.fadeIn(1200);
 }
+
+);
+
+exit.click(
+function()
+{
+  hamburgerMenu.fadeOut(1200);
+}
+
 
 );
